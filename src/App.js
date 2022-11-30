6f985135fe6f2947393c61a90603217693c9ddf6
Hanan-7101 <108216273+Hanan-7101@users.noreply.github.com>
2022-11-30 18:12:02 +0500

    parallax effect responsivnes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,55 @@ import logo from "./assets/Gold.png"
 import logo1 from "./assets/coper.png"
 import Navbar from './components/navbar';
 import SimpleDialogDemo from './components/popup';
-
+import { Box, Fab } from '@mui/material';
+import { useState } from 'react';
+import NavigationIcon from '@mui/icons-material/Navigation';
+import { TroubleshootTwoTone } from '@mui/icons-material';
 const  App=()=> {
+  const [open,setOpen]=useState(TroubleshootTwoTone)
   return (<>
    
     <div className="App">
-   
-      <img  src={logo1}  alt='map'   className='img2' />
-        
-      <img  src={logo}  alt='map'   className='img1' />
-       
+      <div className='water'> 
+      <svg>
+  <filter id="turbulence" x="0" y="0" width="100%" height="100%">
+    <feTurbulence id="sea-filter" numOctaves="3" seed="2" baseFrequency="0.02 0.05"></feTurbulence>
+    <feDisplacementMap scale="20" in="SourceGraphic"></feDisplacementMap>
+  </filter>
+  <animate href="#sea-filter" attributeName="baseFrequency" dur="60s" keyTimes="0;0.5;1" values="0.02 0.06;0.04 0.08;0.02 0.06" repeatCount="indefinite"/>
+</svg>
+
+  
+ 
+     
+      
 </div>
-<div className='l1'>   <SimpleDialogDemo     />              </div>
+<div className='main'>  <img  src={logo1}  alt='map'   className='img2' />
+        
+        <img  src={logo}  alt='map'   className='img1' /></div>
+      <div className='assets-main'>
+        <SimpleDialogDemo     /> 
+        </div> 
+      </div>
+
+
+   
+             <div  className='nav123'>
+              {/* <Box>
+             <Fab variant="extended" size="small" color="primary" aria-label="add" onClick={()=>setOpen(!open)} >
+                    <NavigationIcon sx={{ mr: 1 }} />
+                    Extended
+                </Fab>
+            </Box>
+               
+
+
+            {
+              open ? ( <Navbar/>):(null)
+            } */}
+
+            
+             </div>
 
 
     
--- src/components/popup.js
+++ src/components/popup.js
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
-
+import {
+  MouseParallaxChild,
+  MouseParallaxContainer
+} from "react-parallax-mouse";
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+
+
 import Slide from '@mui/material/Slide';
 import "../App.css"
-import { Tooltip } from '@mui/material';
+import { DialogContentText, DialogTitle, Tooltip } from '@mui/material';
 import logo3 from "../assets/largemarket.png";
 import logo2 from "../assets/waracc.png";
 import logo4 from "../assets/farms.png";
@@ -22,7 +25,7 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-const AlertDialogSlide=( )=> {
+const AlertDialogSlide = () => {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -36,299 +39,393 @@ const AlertDialogSlide=( )=> {
   return (<>
 
 
-  
-    <div className='dia'>
-        <Tooltip title={<span>Enter Details About this building.
-            <span><br/></span>
-         1.
-         <span><br/></span>
-        2.
-        <span><br/></span>
-        3.
-        </span>
-  }
-      enterDelay={600}  >
-        <img alt='y'  src={logo2}   onClick={handleClickOpen} className="btn1"/>
-        {/* <img alt='o'  src={logo3}   onClick={handleClickOpen} className="bl2"/> */}
-     
-      </Tooltip>
-      <Dialog
-      sx={{width:"500px",height:"500px",marginLeft:"30rem"}}
-      
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
+    <MouseParallaxContainer
+      className="parallax"
+
+      globalFactorX={0.3}
+      globalFactorY={0.3}
+      resetOnLeave
+    >
+
+      <MouseParallaxChild
+        className="dia"
       >
-        <DialogTitle>{"Heading"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-      body
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-
-        </DialogActions>
-      </Dialog>
-    </div>
-    <div className='dia2'>
         <Tooltip title={<span>Enter Details About this building.
-            <span><br/></span>
-         1.
-         <span><br/></span>
-        2.
-        <span><br/></span>
-        3.
+          <span><br /></span>
+          1.
+          <span><br /></span>
+          2.
+          <span><br /></span>
+          3.
         </span>
-  }
-      enterDelay={600}  >
-        <img alt='y'  src={logo5}   onClick={handleClickOpen} className="bl2"/>
+        }
+          enterDelay={600}  >
 
-     
-      </Tooltip>
-      <Dialog
-      sx={{width:"500px",height:"500px",marginLeft:"30rem"}}
-      
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
+          <img alt='y' src={logo2} width="100%" onClick={handleClickOpen} className="btn1" />
+          {/* <img alt='o'  src={logo3}   onClick={handleClickOpen} className="bl2"/> */}
+
+        </Tooltip>
+      </MouseParallaxChild>
+    </MouseParallaxContainer>
+    <Dialog
+      sx={{ width: "500px", height: "500px", marginLeft: "30rem" }}
+      overlayStyle={{ backgroundColor: 'transparent' }}
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle>{"Heading"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          body
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+
+      </DialogActions>
+    </Dialog>
+
+    <MouseParallaxContainer
+      className="parallax2"
+
+      globalFactorX={0.3}
+      globalFactorY={0.3}
+      resetOnLeave
+    >
+      <MouseParallaxChild
+        className="dia2"
       >
-        <DialogTitle>{"Heading"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-      body
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
 
-        </DialogActions>
-      </Dialog>
-  </div>
-  <div className='dia3'>
         <Tooltip title={<span>Enter Details About this building.
-            <span><br/></span>
-         1.
-         <span><br/></span>
-        2.
-        <span><br/></span>
-        3.
+          <span><br /></span>
+          1.
+          <span><br /></span>
+          2.
+          <span><br /></span>
+          3.
         </span>
-  }
-      enterDelay={600}  >
-        <img alt='y'  src={logo6}   onClick={handleClickOpen} className="bl3"/>
+        }
+          enterDelay={600}  >
+          <img alt='y' src={logo5} width="110%" onClick={handleClickOpen} className="bl2" />
 
-     
-      </Tooltip>
-      <Dialog
-      sx={{width:"500px",height:"500px",marginLeft:"30rem"}}
-      
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
+
+        </Tooltip>
+      </MouseParallaxChild>
+    </MouseParallaxContainer>
+    <Dialog
+      sx={{ width: "500px", height: "500px", marginLeft: "30rem" }}
+
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle>{"Heading"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          body
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+
+      </DialogActions>
+    </Dialog>
+    <MouseParallaxContainer
+      className="parallax3"
+
+      globalFactorX={0.3}
+      globalFactorY={0.3}
+      resetOnLeave
+    >
+
+      <MouseParallaxChild
+        className="dia3"
       >
-        <DialogTitle>{"Heading"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-      body
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
 
-        </DialogActions>
-      </Dialog>
-  </div>
-  <div className='dia4'>
         <Tooltip title={<span>Enter Details About this building.
-            <span><br/></span>
-         1.
-         <span><br/></span>
-        2.
-        <span><br/></span>
-        3.
+          <span><br /></span>
+          1.
+          <span><br /></span>
+          2.
+          <span><br /></span>
+          3.
         </span>
-  }
-      enterDelay={600}  >
-     
-        <img alt='o'  src={logo3}   onClick={handleClickOpen} className="bl4"/>
-     
-      </Tooltip>
-      <Dialog
-      sx={{width:"500px",height:"500px",marginLeft:"30rem"}}
-      
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>{"Heading"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-      body
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
+        }
+          enterDelay={600}  >
+          <img alt='y' src={logo6} onClick={handleClickOpen} className="bl3" />
 
-        </DialogActions>
-      </Dialog>
-   </div>
-    <div className='dia5'>
+
+        </Tooltip>
+      </MouseParallaxChild>
+    </MouseParallaxContainer>
+    <Dialog
+      sx={{ width: "500px", height: "500px", marginLeft: "30rem" }}
+
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle>{"Heading"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          body
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+
+      </DialogActions>
+    </Dialog>
+    <MouseParallaxContainer
+      className="parallax4"
+
+      globalFactorX={0.3}
+      globalFactorY={0.3}
+      resetOnLeave
+    >
+
+      <MouseParallaxChild
+        className="dia4"
+      >
+
         <Tooltip title={<span>Enter Details About this building.
-            <span><br/></span>
-         1.
-         <span><br/></span>
-        2.
-        <span><br/></span>
-        3.
+          <span><br /></span>
+          1.
+          <span><br /></span>
+          2.
+          <span><br /></span>
+          3.
         </span>
-  }
-      enterDelay={600}  >
-        <img alt='y'  src={logo4}   onClick={handleClickOpen} className="bl5"/>
-    
-     
-      </Tooltip>
-      <Dialog
-      sx={{width:"500px",height:"500px",marginLeft:"30rem"}}
-      
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>{"Heading"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-      body
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
+        }
+          enterDelay={600}  >
 
-        </DialogActions>
-      </Dialog>
-  </div>
-    <div className='dia6'>
+          <img alt='o' src={logo3} width="100%" onClick={handleClickOpen} className="bl4" />
+
+        </Tooltip>
+      </MouseParallaxChild>
+    </MouseParallaxContainer>
+    <Dialog
+      sx={{ width: "500px", height: "500px", marginLeft: "30rem" }}
+
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle>{"Heading"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          body
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+
+      </DialogActions>
+    </Dialog>
+
+    <MouseParallaxContainer
+      className="parallax5"
+
+      globalFactorX={0.4}
+      globalFactorY={0.4}
+      resetOnLeave
+    >
+
+      <MouseParallaxChild
+        className="dia5"
+      >
         <Tooltip title={<span>Enter Details About this building.
-            <span><br/></span>
-         1.
-         <span><br/></span>
-        2.
-        <span><br/></span>
-        3.
+          <span><br /></span>
+          1.
+          <span><br /></span>
+          2.
+          <span><br /></span>
+          3.
         </span>
-  }
-      enterDelay={600}  >
-        <img alt='y'  src={logo8}   onClick={handleClickOpen} className="bl6"/>
-    
-     
-      </Tooltip>
-      <Dialog
-      sx={{width:"500px",height:"500px",marginLeft:"30rem"}}
-      
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>{"Heading"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-      body
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
+        }
+          enterDelay={600}  >
+          <img alt='y' src={logo4} width="100%" onClick={handleClickOpen} className="bl5" />
 
-        </DialogActions>
-      </Dialog>
-  </div>
-  <div className='dia7'>
+
+        </Tooltip>
+      </MouseParallaxChild>
+    </MouseParallaxContainer>
+    <Dialog
+      sx={{ width: "500px", height: "500px", marginLeft: "30rem" }}
+
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle>{"Heading"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          body
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+
+      </DialogActions>
+    </Dialog>
+
+    <MouseParallaxContainer
+      className="parallax6"
+
+      globalFactorX={0.4}
+      globalFactorY={0.4}
+      resetOnLeave
+    >
+
+      <MouseParallaxChild
+        className="dia6"
+      >
         <Tooltip title={<span>Enter Details About this building.
-            <span><br/></span>
-         1.
-         <span><br/></span>
-        2.
-        <span><br/></span>
-        3.
+          <span><br /></span>
+          1.
+          <span><br /></span>
+          2.
+          <span><br /></span>
+          3.
         </span>
-  }
-      enterDelay={600}  >
-        <img alt='y'  src={logo7}   onClick={handleClickOpen} className="bl7"/>
-    
-     
-      </Tooltip>
-      
-      <Dialog
-      sx={{width:"500px",height:"500px",marginLeft:"30rem"}}
-      
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>{"Heading"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-      body
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
+        }
+          enterDelay={600}  >
+          <img alt='y' src={logo8} onClick={handleClickOpen} className="bl6" />
 
-        </DialogActions>
-      </Dialog>
-   </div>
-  <div className='dia8'>
+
+        </Tooltip>
+      </MouseParallaxChild>
+    </MouseParallaxContainer>
+    <Dialog
+      sx={{ width: "500px", height: "500px", marginLeft: "30rem" }}
+
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle>{"Heading"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          body
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+
+      </DialogActions>
+    </Dialog>
+
+    <MouseParallaxContainer
+      className="parallax7"
+
+      globalFactorX={0.4}
+      globalFactorY={0.4}
+      resetOnLeave
+    >
+
+      <MouseParallaxChild
+        className="dia7"
+      >
         <Tooltip title={<span>Enter Details About this building.
-            <span><br/></span>
-         1.
-         <span><br/></span>
-        2.
-        <span><br/></span>
-        3.
+          <span><br /></span>
+          1.
+          <span><br /></span>
+          2.
+          <span><br /></span>
+          3.
         </span>
-  }
-      enterDelay={600}  >
-        <img alt='y'  src={logo9}   onClick={handleClickOpen} className="bl8"/>
-    
-     
-      </Tooltip>
-      
-      <Dialog
-      sx={{width:"500px",height:"500px",marginLeft:"30rem"}}
-      
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>{"Heading"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-      body
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
+        }
+          enterDelay={600}  >
+          <img alt='y' src={logo7} onClick={handleClickOpen} className="bl7" />
 
-        </DialogActions>
-      </Dialog>
-   </div>
-   
-   </>
+
+        </Tooltip>
+      </MouseParallaxChild>
+    </MouseParallaxContainer>
+    <Dialog
+      sx={{ width: "500px", height: "500px", marginLeft: "30rem" }}
+
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle>{"Heading"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          body
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+
+      </DialogActions>
+    </Dialog>
+
+    <MouseParallaxContainer
+      className="parallax8"
+
+      globalFactorX={0.4}
+      globalFactorY={0.4}
+      resetOnLeave
+    >
+
+      <MouseParallaxChild
+        className="dia8"
+      >
+        <Tooltip title={<span>Enter Details About this building.
+          <span><br /></span>
+          1.
+          <span><br /></span>
+          2.
+          <span><br /></span>
+          3.
+        </span>
+        }
+          enterDelay={600}  >
+          <img alt='y' src={logo9} onClick={handleClickOpen} className="bl8" />
+
+
+        </Tooltip>
+      </MouseParallaxChild>
+    </MouseParallaxContainer>
+    <Dialog
+      sx={{ width: "500px", height: "500px", marginLeft: "30rem" }}
+
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle>{"Heading"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          body
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+
+      </DialogActions>
+    </Dialog>
+
+
+  </>
   );
 }
-export default  AlertDialogSlide;
+export default AlertDialogSlide;
